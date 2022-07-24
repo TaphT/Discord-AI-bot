6fd3527a379b79499eba226f13bbbba004457c70
TaphT <109799893+TaphT@users.noreply.github.com>
2022-07-24 08:01:35 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Discord image ai bot
+ 
---
+++ main.py
@@ -0,0 +1,69 @@
+import ast
+import random
+import requests
+import base64
+import discord
+
+working = False
+
+with open("token.py", "r") as f:
+    TOKEN = "insert token"
+
+file_name = ""
+
+
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} is up and running')
+
+
+@client.event
+async def on_message(message):
+    if 'insert.prefix' in message.content:
+        global working
+        craiyon_embed = discord.Embed(title=f"Generating{message.content.replace('insert.prefix','')}", url="https://craiyon.com",
+                                      description="Craiyon is thinking.. (This may take up to 3 minutes)",
+                                      color=0xffae00)
+        wait = await message.reply(embed=craiyon_embed)
+        print(f"Requesting {message.content.replace('insert.prefix','')}")
+        try:
+            if working == False:
+                working = True
+                get_image(message.content.replace('insert.prefix',''))
+                print('Got Image | Sending...')
+                await wait.delete()
+                working = False
+            else:
+                await message.channel.send('Bot in use please wait...')
+
+        except Exception:
+            embed = discord.Embed(title="Error", url="https://craiyon.com",
+                                  description="An Error Occured | This may be too mass requests, consider waiting a few minutes before retrying",
+                                  color=0xffae00)
+            embed=discord.Embed(title="Error", url="https://craiyon.com", description="An Error Occured | This may be too mass requests, consider waiting a few minutes before retrying", color=0xffae00)
+            await message.channel.send(embed=embed)
+        await message.reply(file=discord.File(file_name))
+    if message.content == 'c!help':
+        help_embed = discord.Embed(title="Craiyon Bot", url="https://craiyon.com",
+                              description="Generates Images From https://craiyon.com | Prefix is c!help   `insert.prefix` - Generates an image based on a word  e.g `insert.prefix tree` will generate a tree on https://craiyon.com",
+                              color=0xffae00)
+        await message.channel.send(embed=help_embed)
+
+def get_image(word):
+    global file_name
+    data = {"prompt": word}
+    url = "https://backend.craiyon.com/generate"
+    response = requests.post(url, json=data)
+    file_name = f'images/{word[1:]}{random.randint(0,1000000)}.jpg'
+    image_dict = (ast.literal_eval(response.content.decode('utf-8')))
+    image_string = image_dict['images'][0]
+    dec_data = base64.b64decode(image_string)
+    image_result = open(file_name, 'wb')
+    image_result.write(dec_data)
+
+
+client.run("insert token"")
+
